bf0b4d7271c80121ac440bef445963c3e4236312
Cazacu Ion <cazacioc51@gmail.com>
2024-12-22 16:46:39 +0200

    Placement of Stones/Game Logic

--- project/test1.py
+++ project/test1.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 
+
 BUTTON_COLOR = (240, 217, 181)
 BUTTON_COLOR_HOVER = (255, 196, 137)
 TEXT_COLOR = (0, 0, 0)
@@ -18,7 +19,6 @@ GRID_BACKGROUND_COLOR = (240, 217, 181, 180)
 LINE_COLOR = (0, 0, 0)
 DOT_RADIUS = 5
 DOT_COLOR = (0, 0, 0)
-STONE_RADIUS = 15
 BLACK_STONE_COLOR = (0, 0, 0)
 WHITE_STONE_COLOR = (255, 255, 255)
 
@@ -40,14 +40,12 @@ def menu():
     bg_music.play(-1)
 
     title_font = pygame.font.Font(FONT, 72)
-    button_font = pygame.font.SysFont(FONT, 36)
-
     button_width, button_height = 350, 70
     button_margin = 20
     buttons = [
-        {"text": "Expert : 19 x 19", "size": 19},
-        {"text": "Intermediate : 13 x 13", "size": 13},
-        {"text": "Beginner : 9 x 9", "size": 9},
+        {"text": "Expert : 19 x 19", "size": 19, "stones": 15},
+        {"text": "Intermediate : 13 x 13", "size": 13, "stones": 23},
+        {"text": "Beginner : 9 x 9", "size": 9, "stones": 33},
         {"text": "Quit", "size": None}
     ]
     button_positions = []
@@ -59,9 +57,9 @@ def menu():
 
     running = True
     selected_size = None
+    stones_radius = None
 
     while running:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -87,10 +85,11 @@ def menu():
                     sys.exit()
                 else:
                     selected_size = buttons[i]["size"]
+                    stones_radius = buttons[i]["stones"]
                     running = False
 
         pygame.display.update()
-    return selected_size, screen.get_size()
+    return selected_size, stones_radius, screen.get_size()
 
 def get_grid_size(window_width, window_height):
     min_dim = min(window_width, window_height)
@@ -103,7 +102,7 @@ def get_star_points(grid_lines):
     if grid_lines == 19:
         return [(3, 3), (3, 9), (3, 15), (9, 3), (9, 9), (9, 15), (15, 3), (15, 9), (15, 15)]
     elif grid_lines == 13:
-        return [(3,3), (3, 9), (9, 3), (9, 9), (6, 6)]
+        return [(3, 3), (3, 9), (9, 3), (9, 9), (6, 6)]
     elif grid_lines == 9:
         return [(4, 4)]
 
@@ -137,18 +136,57 @@ def draw_board(screen, grid_size, grid_lines, window_size):
         pygame.draw.line(screen, LINE_COLOR, (position_x, grid_start_y), (position_x, grid_end_y), 2)
 
     draw_star_points(screen, grid_size, grid_lines, grid_start_x, grid_start_y)
+
+    return grid_start_x, grid_start_y, lines_step
+
+def get_neighbours(col, row, grid_lines):
+    neighbours = []
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        new_col, new_row = col + dx, row + dy
+        if 0 <= new_col < grid_lines and 0 <= new_row < grid_lines:
+            neighbours.append((new_col, new_row))
+    return neighbours
+
+def get_group_and_liberties(col, row, player, stones, grid_lines):
+    queue = [(col, row)]
+    group = set()
+    liberties = set()
+
+    while queue:
+        c, r = queue.pop(0)
+        if (c, r) in group:
+            continue
+        group.add((c, r))
+        for neighbour in get_neighbours(c, r, grid_lines):
+            if neighbour in group:
+                continue
+            if neighbour in stones and stones[neighbour] == player:
+                queue.append(neighbour)
+            elif neighbour not in stones:
+                liberties.add(neighbour)
+
+    return group, liberties
+
+def remove_group(group, stones):
+    for pos in group:
+        del stones[pos]
+
 def main():
     pygame.init()
+
+    grid_lines, stones_radius, screen_size = menu()
 
     bg_music = pygame.mixer.Sound(BG_MUSIC)
     bg_music.set_volume(0.3)
     bg_music.play(-1)
-    screen = pygame.display.set_mode((menu()[1]), pygame.RESIZABLE)
+    screen = pygame.display.set_mode(screen_size, pygame.RESIZABLE)
     pygame.display.set_caption("The Game of Go")
     pygame.display.set_icon(ICON)
     window_size = SCREEN_SIZE
 
-    grid_lines = menu()[0]
+    stones = {}
+    current_player = 1  # black
+    grid_size = get_grid_size(window_size[0], window_size[1])
 
     running = True
     while running:
@@ -160,12 +198,38 @@ def main():
                 height = max(event.h, MIN_SCREEN_SIZE[1])
                 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
                 window_size = (width, height)
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                grid_start_x, grid_start_y, lines_step = draw_board(screen, grid_size, grid_lines, window_size)
+                col = round((mouse_x - grid_start_x) / lines_step)
+                row = round((mouse_y - grid_start_y) / lines_step)
+                if 0 <= col < grid_lines and 0 <= row < grid_lines and (col, row) not in stones:
+                    stones[(col, row)] = current_player
+
+                    for neighbour in get_neighbours(col, row, grid_lines):
+                        if neighbour in stones and stones[neighbour] != current_player:
+                            group, liberties = get_group_and_liberties(neighbour[0], neighbour[1], stones[neighbour], stones, grid_lines)
+                            if not liberties:
+                                remove_group(group, stones)
+
+                    group, liberties = get_group_and_liberties(col, row, current_player, stones, grid_lines)
+                    if not liberties:
+                        del stones[(col, row)]
+                    else:
+                        current_player = 3 - current_player
 
         grid_size = get_grid_size(window_size[0], window_size[1])
         background = pygame.transform.scale(BACKGROUND, window_size)
         screen.blit(background, (0, 0))
 
-        draw_board(screen, grid_size, grid_lines, window_size)
+        grid_start_x, grid_start_y, lines_step = draw_board(screen, grid_size, grid_lines, window_size)
+
+        for (col, row), player in stones.items():
+            stone_x_pos = grid_start_x + col * lines_step
+            stone_y_pos = grid_start_y + row * lines_step
+            stone_color = BLACK_STONE_COLOR if player == 1 else WHITE_STONE_COLOR
+            pygame.draw.circle(screen, stone_color, (int(stone_x_pos), int(stone_y_pos)), stones_radius)
+
         pygame.display.update()
 
     pygame.quit()
